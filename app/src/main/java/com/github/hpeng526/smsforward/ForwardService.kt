5761d89e0bf9919c42006442880ced9443fe9d16
yuxiaofei93 <yuxiaofei93@foxmail.com>
2017-08-20 12:32:38 +0800

    换成前台进程处理短信，另外一个进程做守护 于晓飞

--- app/src/main/java/com/github/hpeng526/smsforward/ForwardService.kt
+++ app/src/main/java/com/github/hpeng526/smsforward/ForwardService.kt
@@ -1,14 +1,9 @@
 package com.github.hpeng526.smsforward
 
-import android.app.Notification
 import android.app.Service
-import android.content.Intent
+import android.content.*
 import android.os.IBinder
-import android.content.IntentFilter
 import android.util.Log
-import android.app.PendingIntent
-import android.graphics.BitmapFactory
-import android.graphics.Bitmap
 
 
 class ForwardService : Service() {
@@ -27,40 +22,14 @@ class ForwardService : Service() {
         registerReceiver(receiver, filter)
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("ForwardService", "start")
+        return START_STICKY
+    }
+
     override fun onDestroy() {
         Log.d(this.javaClass.name, "ForwardService destroy")
         unregisterReceiver(receiver)
         super.onDestroy()
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent?.action == Const.START_ACTION) {
-
-            Log.d("ForwardService", "Receive from " + intent.toString())
-
-            val notificationIntent = Intent(this, MainActivity::class.java)
-            notificationIntent.action = Const.MAIN_ACTION
-            notificationIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-
-            val icon = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
-
-            val notification = Notification.Builder(this)
-                    .setContentTitle("SMSForward")
-                    .setTicker("sms Forwarder")
-                    .setContentText("Monitoring...")
-                    .setOngoing(true)
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
-                    .setContentIntent(pendingIntent)
-                    .build()
-
-            startForeground(-1982, notification)
-        } else if (intent?.action == Const.STOP_ACTION) {
-            Log.d("ForwardService", "Received Stop Foreground Intent")
-            stopForeground(true)
-            stopSelf()
-        }
-        return START_STICKY
     }
 }
